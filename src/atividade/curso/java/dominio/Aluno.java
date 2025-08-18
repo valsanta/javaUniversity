99a607216ea016a0974918160a6110d0f9d68096
Valquiria Melo <valquiriameloqa@gmail.com>
2025-08-18 16:12:09 -0300

    atualizacao sprint 1

---
+++ src/atividade/curso/java/dominio/Aluno.java
@@ -0,0 +1,5 @@
+package atividade.curso.java.dominio;
+
+public class Aluno {
+
+}
---
+++ src/atividade/curso/java/main.java
@@ -0,0 +1,18 @@
+package atividade.curso.java;
+
+public class main {
+    public static void main(String[] args) {
+
+        String nome = "Alice";
+        //Notas: Matemática, Física, Química
+        int[] notas = {85, 92, 78};
+
+        //Dados foram armazenados nas variáveis
+        System.out.println("Dados armazenados");
+        //Exibir notas  solicitadas
+        System.out.println("Notas da aluna " + nome + ":");
+        System.out.println("Matemática :" + notas[0]);
+        System.out.println("Física :" + notas[1]);
+        System.out.println("Química :" + notas[2]);
+    }
+    }
